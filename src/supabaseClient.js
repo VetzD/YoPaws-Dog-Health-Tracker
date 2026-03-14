890e534e22ae7842f62ba2c6b52f3005c2c25e6c
VetzD <31454072+VetzD@users.noreply.github.com>
2026-03-15 01:20:06 +1100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_PROJECT_URL"
-const supabaseAnonKey = "YOUR_PUBLIC_ANON_KEY"
+const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
