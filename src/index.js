3019ffd587a34ed7e73ac1de7dd2aa47329424cc
VetzD <31454072+VetzD@users.noreply.github.com>
2026-03-15 13:04:36 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import YoPawsHealthTracker from "./App";
+import "./styles.css";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <YoPawsHealthTracker />
+  </React.StrictMode>
 );
